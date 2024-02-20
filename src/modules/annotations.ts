685bbb28d7c8dbcc91f72b0e64e5a033eefc03ea
zhoulibin@xiaoxin <125377187@qq.com>
2024-02-20 21:56:26 +0800

    1

--- src/modules/annotations.ts
+++ src/modules/annotations.ts
@@ -1,3 +1,5 @@
+
+
 export class Annotations {
   initMenu() {
     if (Zotero.platformMajorVersion >= 102) {
@@ -9,16 +11,21 @@ export class Annotations {
       "createAnnotationContextMenu",
       (event) => {
         const { reader, params, append } = event;
-        const command = (label: string) => {
-          for (const id of params.ids) {
-            const annotation = reader._item
-              .getAnnotations()
-              .filter(function (e) {
-                return e.key == id;
-              })[0];
-            annotation.addTag(label, 0);
-          }
-        };
+		addon.data.ztoolkit.log(event)
+        const command = (label: string) =>
+          function () {
+			addon.data.ztoolkit.log(label)
+            for (const id of params.ids) {
+              const annotation = reader._item
+                .getAnnotations()
+                .filter(function (e) {
+                  return e.key == id;
+                })[0];
+		// addon.data.ztoolkit.log(annotation)
+              annotation.addTag(label, 0);
+			  annotation.saveTx()
+            }
+          };
         for (const tag of [
           "研究目的",
           "研究假设",
@@ -36,3 +43,4 @@ export class Annotations {
     );
   }
 }
+ 
