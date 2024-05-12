9f1df64c343cf7e889a01c6bb0a8616dd1bc8ae4
zhoulibin@xiaoxin <125377187@qq.com>
2024-05-12 08:39:16 +0800

    增加多种配置

--- src/modules/highlightWords.ts
+++ src/modules/highlightWords.ts
@@ -14,7 +14,7 @@ import { waitUtilAsync } from "../utils/wait";
 import { getPref, setPref } from "../utils/prefs";
 import { compare } from "../utils/sort";
 import { col } from "../utils/action-col";
-import { text } from "d3";
+import { text, timeout } from "d3";
 
 function register() {
   Zotero.Reader.registerEventListener(
@@ -39,7 +39,62 @@ const readerButtonCSS = `
     vertical-align: top;
     width: 16px;
     border-radius:4px;
-  }  `;
+  } 
+  #${config.addonRef}-search-button-pop *{
+    font-size:18px
+  }
+  #${config.addonRef}-search-button-pop .nav{
+     display:none;
+  } 
+  #${config.addonRef}-search-button-pop.enter .nav{
+     display:flex;
+     flex-direction:row;
+     justify-content: flex-end;
+  }
+
+  #${config.addonRef}-search-button-pop textarea{
+     display:none
+  } 
+  #${config.addonRef}-search-button-pop.enter textarea{
+     display:block
+  } 
+  `;
+
+class HighlightWords {
+  words: string[] = [];
+  index = 0;
+
+  constructor() {
+    this.load();
+  }
+  load() {
+    this.words = JSON.parse((getPref("HighlightWords") as string) || `[""]`);
+    this.index = (getPref("HighlightIndex") as number) || 0;
+  }
+  save() {
+    this.words = uniqueBy(
+      this.words.filter((word) => word !== ""),
+      (f) => f,
+    );
+    if (this.words.length == 0) this.words.push("");
+    setPref("HighlightWords", JSON.stringify(this.words));
+    setPref("HighlightIndex", this.index);
+  }
+  get(index: number = this.index) {
+    if (index >= this.words.length || index < 0) {
+      return "";
+    }
+    return this.words[index];
+  }
+  set(value: string, index: number = this.index) {
+    if (index >= this.words.length) {
+      this.words.push(value);
+    } else {
+      this.words[index] = value;
+    }
+  }
+}
+const highlightWords = new HighlightWords();
 
 function readerToolbarCallback(
   event: Parameters<_ZoteroTypes.Reader.EventHandler<"renderToolbar">>[0],
@@ -58,9 +113,8 @@ function readerToolbarCallback(
     },
     root,
   );
-  let searchText = "";
-  // reader?._iframe?.contentDocument?.querySelector("iframe")?.
-  //     window.addEventListener('scroll', throttleHandleScroll);
+  let searchText = highlightWords.get();
+  let highlightIndex = highlightWords.index;
   const throttleSearch = Zotero.Utilities.throttle(() => {
     ztoolkit.log("滚动2");
     search();
@@ -69,6 +123,10 @@ function readerToolbarCallback(
     ?.contentDocument as Document;
   let running = false;
   let onceMore = false;
+  let height = 1000;
+  let popDiv: HTMLElement | undefined = undefined;
+  const colors = memFixedTagColors().map((a) => a.color);
+
   function search() {
     if (!popDiv) return;
 
@@ -78,22 +136,10 @@ function readerToolbarCallback(
     }
     running = true;
 
-    const ss = uniqueBy(
-      searchText
-        .split("\n")
-        .filter((f) => f)
-        .filter((f) => f != " "),
-      (a) => a,
-    );
-    // if (ss.length == 0) return
-    // ztoolkit.log(searchText, ss)
-    // ztoolkit.log("search",
-    //     pdfDoc?.querySelectorAll("span"),
-    //     pdfDoc
-    // )
-    const colors = memFixedTagColors().map((a) => a.color);
+    const { ss, ssr, getColor, getSpan } = getFormatStr();
+
     for (const span of pdfDoc.querySelectorAll("span[role=presentation]")) {
-      if (span.screenY < -100 || span.screenY > 1000) continue;
+      if (span.screenY < -height || span.screenY > height * 2) continue;
       let html = span.getAttribute("data-text");
       if (!html) {
         html = span.textContent;
@@ -102,24 +148,45 @@ function readerToolbarCallback(
         }
       }
       if (!html) continue;
-      for (let index = 0; index < ss.length; index++) {
-        const item = ss[index];
-        const r = new RegExp(item, "ig");
-        html = html.replace(
-          r,
-          (a) =>
-            `<span style='background:${colors[index]};box-shadow: 0 0 10px rgba(180, 0, 170, 1);box-sizing: content-box;border: 2px solid #fff;'>${a}</span>`,
-        );
-        //
+      if (ss.length > 0) {
+        html = html.replace(ssr, getSpan);
       }
-      // ztoolkit.log(text, html)
-      // if (html.includes("<span style='background:")){}
+
       span.innerHTML = html;
     }
 
-    running = false;
-    if (onceMore) search();
+    setTimeout(() => {
+      running = false;
+      if (onceMore) search();
+    }, 1000);
   }
+  function getFormatStr() {
+    const ss = uniqueBy(
+      searchText
+        .split("\n")
+        .filter((f) => f)
+        .filter((f) => f != " "),
+      (a) => a,
+    );
+    const ssr = new RegExp(
+      `(?:${ss.map((a) => a.replace(/[|]/g, ".")).join("|")})`,
+      "ig",
+    );
+    const rs = ss.map((s) => new RegExp(s, "ig"));
+    const getColor = (str: string) => {
+      for (let index = 0; index < rs.length; index++) {
+        const r = rs[index];
+        if (r.test(str)) {
+          return colors[index] || "rgba(180, 0, 170, 1)";
+        }
+      }
+      return "rgba(180, 0, 170, 1)";
+    };
+    const getSpan = (a: string) =>
+      `<span style='background:${getColor(a)};box-shadow: 0 0 10px rgba(180, 0, 170, 1);box-sizing: content-box;border: 2px solid #fff;'>${a}</span>`;
+    return { ss, ssr, getColor, getSpan };
+  }
+
   function clearSearch() {
     for (const span of pdfDoc.querySelectorAll("span[role=presentation]")) {
       const html = span.getAttribute("data-text");
@@ -128,7 +195,6 @@ function readerToolbarCallback(
   }
 
   const debounceSearch = Zotero.Utilities.debounce(search, 300);
-  let popDiv: HTMLElement | undefined = undefined;
 
   const btn = ztoolkit.UI.createElement(doc, "div", {
     namespace: "html",
@@ -140,8 +206,7 @@ function readerToolbarCallback(
     ],
     properties: {
       tabIndex: -1,
-      title: "查找",
-      // textContent: "查找"
+      title: "高亮",
     },
     listeners: [
       {
@@ -152,6 +217,8 @@ function readerToolbarCallback(
           pdfDoc = reader?._iframe?.contentDocument?.querySelector("iframe")
             ?.contentDocument as Document;
           if (!pdfDoc || !pdfDoc.querySelector("#viewerContainer")) return;
+          height = (pdfDoc.querySelector("#viewerContainer")! as HTMLElement)
+            .offsetHeight;
 
           const evm = ev as MouseEvent;
           const div = evm.target as HTMLElement;
@@ -170,111 +237,206 @@ function readerToolbarCallback(
             div.style.background = "#ddd";
             searchText = (getPref("search-words") as string) || "";
             debounceSearch();
-            popDiv = ztoolkit.UI.appendElement(
-              {
-                tag: "div",
-                id: `${config.addonRef}-search-button`,
-                properties: { placeholder: "一行一个单词" },
-                styles: {
-                  position: "fixed",
-                  // left: div.offsetLeft - 100 + "px",
-                  right: "10px",
-                  top: div.offsetTop + div.offsetHeight + "px",
-                  zIndex: "9999",
-                  minWidth: "160px",
-                  background: "#ddd",
-                  padding: "10px",
-                  borderRadius: "5px",
-                },
-                children: [
-                  {
-                    tag: "textarea",
-                    namespace: "html",
-                    styles: {
-                      width: "100%",
-                      height: "5em",
-                      overflowY: "scroll",
-                    },
-                    properties: { value: searchText },
-                    listeners: [
-                      {
-                        type: "keyup",
-                        listener: (ev: Event) => {
-                          ev.stopPropagation;
-                          const ta = ev.target as HTMLTextAreaElement;
-                          searchText = ta.value;
-                          setPref("search-words", searchText);
-                          debounceSearch();
-                        },
-                        options: { capture: true },
-                      },
-                    ],
-                  },
-                ],
-              },
-              root,
-            ) as HTMLElement;
+            createPopDiv(div);
           }
         },
       },
     ],
   });
   append(btn);
-  const a = ztoolkit.UI.createElement(doc, "div", {
-    id: `${config.addonRef}-search-button`,
-    listeners: [
+  let textArea: HTMLTextAreaElement;
+  let show: HTMLDivElement;
+  function createPopDiv(div: HTMLElement) {
+    popDiv = ztoolkit.UI.appendElement(
       {
-        type: "click",
-        listener: (ev: Event) => {
-          ztoolkit.log(ev);
+        tag: "div",
+        id: `${config.addonRef}-search-button-pop`,
+        properties: { placeholder: "一行一个单词" },
+        styles: {
+          position: "fixed",
+          // left: div.offsetLeft - 300 + "px",
+          right: "10px",
+          // right: "300px",
+          top: div.offsetTop + div.offsetHeight + 5 + "px",
+          // top: "0px",
+          zIndex: "9999",
+          minWidth: "280px",
+          background: "#ddd",
+          padding: "1px",
+          borderRadius: "5px",
+          display: "flex",
+          flexDirection: "column",
         },
-      },
-    ],
-    children: [
-      {
-        tag: "button",
-        namespace: "html",
-        id: `${config.addonRef}-search-button-a`,
-        classList: ["toolbarButton", `${config.addonRef}-search-button-a`],
-        properties: {
-          tabIndex: -1,
-          title: "查找",
-          textContent: "查找",
-        },
-      },
-    ],
-  });
-  if (a) {
-    // append(a)
-    return;
-  }
-  doc.getElementById(`${config.addonRef}-search-button-style`)?.remove();
-  append(
-    ztoolkit.UI.createElement(doc, "button", {
-      namespace: "html",
-      id: `${config.addonRef}-search-button`,
-      classList: ["toolbarButton", `${config.addonRef}-search-menu`],
-      properties: {
-        tabIndex: -1,
-        title: "查找",
-      },
-      listeners: [
-        {
-          type: "click",
-          listener: (ev: Event) => {
-            ztoolkit.log(ev);
+        children: [
+          {
+            tag: "div",
+
+            classList: ["nav"],
+            children: [
+              {
+                tag: "div",
+                styles: {
+                  padding: "5px",
+                  background: "#bbb",
+                  margin: "2px",
+                  cursor: "pointer",
+                },
+                properties: { textContent: "<" },
+                listeners: [
+                  {
+                    type: "click",
+                    listener: (ev) => {
+                      highlightIndex--;
+                      searchText = highlightWords.get(highlightIndex);
+                      textArea.value = searchText;
+                      if (highlightIndex < 0) {
+                        highlightIndex = highlightWords.words.length;
+                        highlightWords.index = 0;
+                      } else highlightWords.index = highlightIndex;
+                      showHighlightIndex();
+                    },
+                  },
+                ],
+              },
+              {
+                tag: "div",
+                styles: { padding: "5px", background: "#bbb", margin: "2px" },
+                properties: { textContent: "x" },
+                listeners: [
+                  {
+                    type: "click",
+                    listener: (ev) => {
+                      searchText = "";
+                      highlightWords.set("", highlightIndex);
+                      highlightWords.save();
+                      if (highlightIndex == highlightWords.words.length) {
+                        highlightIndex = 0;
+                      }
+                      searchText = highlightWords.get(highlightIndex);
+                      textArea.value = searchText;
+                      showHighlightIndex();
+                    },
+                  },
+                ],
+              },
+              {
+                tag: "div",
+                styles: { padding: "5px", background: "#bbb", margin: "2px" },
+                properties: { textContent: ">" },
+                listeners: [
+                  {
+                    type: "click",
+                    listener: (ev) => {
+                      highlightIndex++;
+                      searchText = highlightWords.get(highlightIndex);
+                      textArea.value = searchText;
+
+                      if (highlightIndex > highlightWords.words.length) {
+                        highlightIndex = 0;
+                        searchText = highlightWords.get(highlightIndex);
+                        textArea.value = searchText;
+                        highlightWords.index = 0;
+                      } else if (
+                        highlightIndex == highlightWords.words.length
+                      ) {
+                        highlightWords.index = 0;
+                      } else {
+                        highlightWords.index = highlightIndex;
+                      }
+                      showHighlightIndex();
+                    },
+                  },
+                ],
+              },
+              {
+                tag: "div",
+                id: `${config.addonRef}-search-button-highlightIndex`,
+                properties: {
+                  textContent: `${highlightIndex}/${highlightWords.words.length}`,
+                },
+              },
+            ],
           },
-        },
-      ],
-    }),
-  );
-  append(
-    ztoolkit.UI.createElement(doc, "style", {
-      id: `${config.addonRef}-search-button-style`,
-      namespace: "html",
-      properties: {
-        textContent: readerButtonCSS,
+          {
+            tag: "textarea",
+            namespace: "html",
+            id: `${config.addonRef}-search-button-textarea`,
+            styles: {
+              width: "100%",
+              height: "5em",
+              overflowY: "scroll",
+            },
+            properties: {
+              value: searchText,
+              placeholder: "一行一个单词\n可以使用正则\n忽略大小写",
+            },
+            listeners: [
+              {
+                type: "input",
+                listener: (ev: Event) => {
+                  ev.stopPropagation;
+                  const ta = ev.target as HTMLTextAreaElement;
+                  searchText = ta.value;
+                  highlightWords.set(searchText, highlightIndex);
+                  highlightWords.save();
+                  showHighlightIndex();
+                },
+                options: { capture: true },
+              },
+            ],
+          },
+          {
+            tag: "div",
+            id: `${config.addonRef}-search-button-pop-show`,
+            classList: ["show"],
+            properties: { textContent: "" },
+          },
+        ],
+        listeners: [
+          {
+            type: "mouseover",
+            listener: (ev: Event) => {
+              ev.stopPropagation;
+              if (!popDiv?.classList.contains("enter"))
+                popDiv?.classList.add("enter");
+              if (timeout) {
+                clearTimeout(timeout);
+                timeout = undefined;
+              }
+            },
+            options: { capture: true },
+          },
+          {
+            type: "mouseout",
+            listener: (ev: Event) => {
+              ev.stopPropagation;
+              timeout = setTimeout(() => {
+                if (popDiv?.classList.contains("enter"))
+                  popDiv?.classList.remove("enter");
+              }, 1000);
+            },
+            options: { capture: true },
+          },
+        ],
       },
-    }),
-  );
+      root,
+    ) as HTMLElement;
+    let timeout: NodeJS.Timeout | undefined;
+    textArea = popDiv.querySelector("textarea")!;
+    show = popDiv.querySelector(`#${config.addonRef}-search-button-pop-show`)!;
+    showHighlightIndex();
+  }
+  function showHighlightIndex() {
+    popDiv!.querySelector(
+      `#${config.addonRef}-search-button-highlightIndex`,
+    )!.textContent = `${highlightIndex + 1}/${highlightWords.words.length}`;
+    const st = uniqueBy(
+      searchText.split("\n").filter((s) => s),
+      (s) => s,
+    );
+    const { ss, ssr, getColor, getSpan } = getFormatStr();
+
+    show.innerHTML = st.map((a) => getSpan(a)).join("");
+    debounceSearch();
+  }
 }
